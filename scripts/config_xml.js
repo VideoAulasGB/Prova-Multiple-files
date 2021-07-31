c37e53093257774bf6df5c04de7986dce3e5c184
VideoAulasGB <88210404+VideoAulasGB@users.noreply.github.com>
2021-07-31 20:16:15 +0200

    Add files via upload

---
+++ scripts/config_xml.js
@@ -0,0 +1,55 @@
+var TSC = TSC || {};
+
+TSC.embedded_config_xml = '<x:xmpmeta tsc:version="2.0.1" xmlns:x="adobe:ns:meta/" xmlns:tsc="http://www.techsmith.com/xmp/tsc/">\
+   <rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:xmp="http://ns.adobe.com/xap/1.0/" xmlns:xmpDM="http://ns.adobe.com/xmp/1.0/DynamicMedia/" xmlns:xmpG="http://ns.adobe.com/xap/1.0/g/" xmlns:xmpMM="http://ns.adobe.com/xap/1.0/mm/" xmlns:tscDM="http://www.techsmith.com/xmp/tscDM/" xmlns:tscIQ="http://www.techsmith.com/xmp/tscIQ/" xmlns:tscHS="http://www.techsmith.com/xmp/tscHS/" xmlns:stDim="http://ns.adobe.com/xap/1.0/sType/Dimensions#" xmlns:stFnt="http://ns.adobe.com/xap/1.0/sType/Font#" xmlns:exif="http://ns.adobe.com/exif/1.0" xmlns:dc="http://purl.org/dc/elements/1.1/">\
+      <rdf:Description dc:date="2021-07-31 08:11:20 " dc:source="Camtasia,21.0.5,enu" dc:title="Prova 5" tscDM:originId="DA75F26B-0494-4D67-A615-A8946BFD3DA2" tscDM:project="Prova 4">\
+         <xmpDM:duration xmpDM:scale="1/1000" xmpDM:value="250833"/>\
+         <xmpDM:videoFrameSize stDim:unit="pixel" stDim:h="1080" stDim:w="1920"/>\
+         <tsc:langName>\
+            <rdf:Bag>\
+               <rdf:li xml:lang="en-US">English</rdf:li></rdf:Bag>\
+         </tsc:langName>\
+         <xmpDM:Tracks>\
+            <rdf:Bag>\
+               <rdf:li>\
+                  <rdf:Description xmpDM:trackType="Hotspot" xmpDM:frameRate="f1000" xmpDM:trackName="Hotspots">\
+                     <xmpDM:markers>\
+                        <rdf:Seq>\
+                           <rdf:li><rdf:Description xmpDM:label="1" xmpDM:startTime="0" xmpDM:duration="8770" tscDM:file="Prova 5-1.mp4" tscDM:boundingPoly="50,314;317,314;317,415;50,415;" tscDM:rotate="0.000000" tscHS:pause="1" tscHS:jumpFile="Prova 5-2.mp4" tscHS:jumpTime="0"/></rdf:li><rdf:li><rdf:Description xmpDM:label="2" xmpDM:startTime="0" xmpDM:duration="8770" tscDM:file="Prova 5-1.mp4" tscDM:boundingPoly="50,440;317,440;317,557;50,557;" tscDM:rotate="0.000000" tscHS:pause="1" tscHS:jumpFile="Prova 5-5.mp4" tscHS:jumpTime="0"/></rdf:li><rdf:li><rdf:Description xmpDM:label="3" xmpDM:startTime="0" xmpDM:duration="20330" tscDM:file="Prova 5-2.mp4" tscDM:boundingPoly="0,0;1920,0;1920,1080;0,1080;" tscDM:rotate="0.000000" tscHS:pause="0"/></rdf:li><rdf:li><rdf:Description xmpDM:label="4" xmpDM:startTime="18270" xmpDM:duration="1230" tscDM:file="Prova 5-2.mp4" tscDM:boundingPoly="1185,555;1876,555;1876,722;1185,722;" tscDM:rotate="0.000000" tscHS:pause="1" tscHS:jumpFile="Prova 5-6.mp4" tscHS:jumpTime="0"/></rdf:li><rdf:li><rdf:Description xmpDM:label="5" xmpDM:startTime="17270" xmpDM:duration="3060" tscDM:file="Prova 5-2.mp4" tscDM:boundingPoly="1140,734;1920,734;1920,986;1140,986;" tscDM:rotate="0.000000" tscHS:pause="0" tscHS:jumpFile="Prova 5-4.mp4" tscHS:jumpTime="0"/></rdf:li><rdf:li><rdf:Description xmpDM:label="6" xmpDM:startTime="0" xmpDM:duration="300" tscDM:file="Prova 5-3.mp4" tscDM:boundingPoly="0,0;1920,0;1920,1080;0,1080;" tscDM:rotate="0.000000" tscHS:pause="0"/></rdf:li><rdf:li><rdf:Description xmpDM:label="7" xmpDM:startTime="0" xmpDM:duration="130" tscDM:file="Prova 5-3.mp4" tscDM:boundingPoly="1140,734;1920,734;1920,986;1140,986;" tscDM:rotate="0.000000" tscHS:pause="1" tscHS:jumpFile="Prova 5-4.mp4" tscHS:jumpTime="0"/></rdf:li><rdf:li><rdf:Description xmpDM:label="8" xmpDM:startTime="0" xmpDM:duration="19330" tscDM:file="Prova 5-4.mp4" tscDM:boundingPoly="0,0;1920,0;1920,1080;0,1080;" tscDM:rotate="0.000000" tscHS:pause="1"/></rdf:li><rdf:li><rdf:Description xmpDM:label="9" xmpDM:startTime="14330" xmpDM:duration="5000" tscDM:file="Prova 5-4.mp4" tscDM:boundingPoly="1599,845;1901,845;1901,928;1599,928;" tscDM:rotate="0.000000" tscHS:pause="1" tscHS:jumpFile="Prova 5-1.mp4" tscHS:jumpTime="0"/></rdf:li><rdf:li><rdf:Description xmpDM:label="10" xmpDM:startTime="0" xmpDM:duration="42000" tscDM:file="Prova 5-5.mp4" tscDM:boundingPoly="0,0;1920,0;1920,1080;0,1080;" tscDM:rotate="0.000000" tscHS:pause="1"/></rdf:li><rdf:li><rdf:Description xmpDM:label="11" xmpDM:startTime="27030" xmpDM:duration="5000" tscDM:file="Prova 5-5.mp4" tscDM:boundingPoly="1609,851;1904,851;1904,928;1609,928;" tscDM:rotate="0.000000" tscHS:pause="1" tscHS:jumpFile="Prova 5-1.mp4" tscHS:jumpTime="0"/></rdf:li><rdf:li><rdf:Description xmpDM:label="12" xmpDM:startTime="27030" xmpDM:duration="5000" tscDM:file="Prova 5-5.mp4" tscDM:boundingPoly="1609,780;1904,780;1904,857;1609,857;" tscDM:rotate="0.000000" tscHS:pause="1" xmpDM:location="https://videoaulasgb.github.io/Prova4/" tscHS:newWindow="0"/></rdf:li><rdf:li><rdf:Description xmpDM:label="13" xmpDM:startTime="0" xmpDM:duration="67000" tscDM:file="Prova 5-6.mp4" tscDM:boundingPoly="0,0;1920,0;1920,1080;0,1080;" tscDM:rotate="0.000000" tscHS:pause="1"/></rdf:li><rdf:li><rdf:Description xmpDM:label="14" xmpDM:startTime="62000" xmpDM:duration="5000" tscDM:file="Prova 5-6.mp4" tscDM:boundingPoly="1577,578;1885,578;1885,666;1577,666;" tscDM:rotate="0.000000" tscHS:pause="1" tscHS:jumpFile="Prova 5-3.mp4" tscHS:jumpTime="0"/></rdf:li></rdf:Seq>\
+                     </xmpDM:markers>\
+                  </rdf:Description>\
+               </rdf:li>\
+               <rdf:li>\
+                  <rdf:Description xmpDM:trackType="TableOfContents" xmpDM:frameRate="f1000" xmpDM:trackName="Table of Contents">\
+                     <xmpDM:markers>\
+                        <rdf:Seq>\
+                           <rdf:li><rdf:Description xmpDM:name="Introduction" tscDM:file="Prova 5-0.mp4" xmpDM:startTime="0" tscDM:image="Prova_5_Thumbnails.png" tscDM:imageindex="0" tscDM:imageoffset="0" tscDM:imagerect="0, 0, 75, 42"/></rdf:li><rdf:li><rdf:Description xmpDM:name="SCELTA" tscDM:file="Prova 5-1.mp4" xmpDM:startTime="0" tscDM:image="Prova_5_Thumbnails.png" tscDM:imageindex="1" tscDM:imageoffset="0" tscDM:imagerect="75, 0, 75, 42"/></rdf:li><rdf:li><rdf:Description xmpDM:name="Opzione A" tscDM:file="Prova 5-2.mp4" xmpDM:startTime="0" tscDM:image="Prova_5_Thumbnails.png" tscDM:imageindex="2" tscDM:imageoffset="0" tscDM:imagerect="150, 0, 75, 42"/></rdf:li><rdf:li><rdf:Description xmpDM:name="Vai A ExE 1" tscDM:file="Prova 5-3.mp4" xmpDM:startTime="0" tscDM:image="Prova_5_Thumbnails.png" tscDM:imageindex="3" tscDM:imageoffset="0" tscDM:imagerect="225, 0, 75, 42"/></rdf:li><rdf:li><rdf:Description xmpDM:name="Salta EXE 1" tscDM:file="Prova 5-4.mp4" xmpDM:startTime="0" tscDM:image="Prova_5_Thumbnails.png" tscDM:imageindex="4" tscDM:imageoffset="0" tscDM:imagerect="300, 0, 75, 42"/></rdf:li><rdf:li><rdf:Description xmpDM:name="Opzione B" tscDM:file="Prova 5-5.mp4" xmpDM:startTime="0" tscDM:image="Prova_5_Thumbnails.png" tscDM:imageindex="5" tscDM:imageoffset="0" tscDM:imagerect="375, 0, 75, 42"/></rdf:li><rdf:li><rdf:Description xmpDM:name="EXEMPLO 1" tscDM:file="Prova 5-6.mp4" xmpDM:startTime="0" tscDM:image="Prova_5_Thumbnails.png" tscDM:imageindex="6" tscDM:imageoffset="0" tscDM:imagerect="450, 0, 75, 42"/></rdf:li></rdf:Seq>\
+                     </xmpDM:markers>\
+                  </rdf:Description>\
+               </rdf:li>\
+            </rdf:Bag>\
+         </xmpDM:Tracks>\
+         <tscDM:controller>\
+            <rdf:Description xmpDM:name="tscplayer">\
+               <tscDM:parameters>\
+                  <rdf:Bag>\
+                     <rdf:li xmpDM:name="autohide" xmpDM:value="true"/><rdf:li xmpDM:name="autoplay" xmpDM:value="true"/><rdf:li xmpDM:name="loop" xmpDM:value="false"/><rdf:li xmpDM:name="searchable" xmpDM:value="true"/><rdf:li xmpDM:name="captionsenabled" xmpDM:value="false"/><rdf:li xmpDM:name="sidebarenabled" xmpDM:value="false"/><rdf:li xmpDM:name="unicodeenabled" xmpDM:value="false"/><rdf:li xmpDM:name="backgroundcolor" xmpDM:value="000000"/><rdf:li xmpDM:name="sidebarlocation" xmpDM:value="left"/><rdf:li xmpDM:name="endaction" xmpDM:value="stop"/><rdf:li xmpDM:name="endactionparam" xmpDM:value="true"/><rdf:li xmpDM:name="locale" xmpDM:value="en-US"/></rdf:Bag>\
+               </tscDM:parameters>\
+               <tscDM:controllerText>\
+                  <rdf:Bag>\
+                  </rdf:Bag>\
+               </tscDM:controllerText>\
+            </rdf:Description>\
+         </tscDM:controller>\
+         <tscDM:contentList>\
+            <rdf:Description>\
+               <tscDM:files>\
+                  <rdf:Seq>\
+                     <rdf:li xmpDM:name="0" xmpDM:value="Prova 5-0.mp4"/><rdf:li xmpDM:name="1" xmpDM:value="Prova 5-1.mp4"/><rdf:li xmpDM:name="2" xmpDM:value="Prova 5-2.mp4"/><rdf:li xmpDM:name="3" xmpDM:value="Prova 5-3.mp4"/><rdf:li xmpDM:name="4" xmpDM:value="Prova 5-4.mp4"/><rdf:li xmpDM:name="5" xmpDM:value="Prova 5-5.mp4"/><rdf:li xmpDM:name="6" xmpDM:value="Prova 5-6.mp4"/><rdf:li xmpDM:name="7" xmpDM:value="Prova_5_Thumbnails.png"/></rdf:Seq>\
+               </tscDM:files>\
+            </rdf:Description>\
+         </tscDM:contentList>\
+      </rdf:Description>\
+   </rdf:RDF>\
+</x:xmpmeta>';
+TSC.embedded_playlist = 'Prova 5-0.mp4\nProva 5-1.mp4\nProva 5-2.mp4\nProva 5-3.mp4\nProva 5-4.mp4\nProva 5-5.mp4\nProva 5-6.mp4\n';
